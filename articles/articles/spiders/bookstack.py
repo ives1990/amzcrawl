4b5fefa4967ea6c0483dce45d3c5ca77af9fa5b5
ives <ives@patazon.net>
2019-09-17 11:02:17 +0800

    init bookstack spider

---
+++ articles/articles/spiders/bookstack.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from articles.items import ArticlesItem
+
+class BookstackSpider(scrapy.Spider):
+    name = 'bookstack'
+    allowed_domains = ['www.bookstack.cn']
+    start_urls = ['http://www.bookstack.cn/explore?tab=recommend']
+
+    def parse(self, response):
+        
+        items = []
+        sites = response.css('.manual-list')
+        for dl in sites.css('dl.manual-item-standard'):
+            book = dl.css('.recommend-book')
+            item = ArticlesItem()
+            item['name'] = dl.css('.name::text').extract_first().strip()
+            item['label'] = book.css('::attr(href)').extract_first().split('/')[-1]
+            item['cover'] = book.css('img::attr(src)').extract_first().strip()
+            item['author'] = 'bookstack'
+            item['description'] = ''
+            item['tags'] = []
+            items.append(item)
+
+        return items
